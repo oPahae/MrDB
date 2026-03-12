b6d8f94d8617b07bba87a0ccec1914f59dd7e254
pahae <151317006+oPahae@users.noreply.github.com>
2026-03-12 17:30:51 +0000

    Create cmds.bash

---
+++ cmds.bash
@@ -0,0 +1,2 @@
+mysql -h mysql-1d4be475-mrdb.i.aivencloud.com -P 12555 -u avnadmin -p defaultdb --ssl-mode=REQUIRED < C:\mrdb.sql
+mysql --user avnadmin --password=AVNS_GR0oJ3YgfGs_1NLOOT- --host mysql-1d4be475-mrdb.i.aivencloud.com --port 12555 defaultdb
